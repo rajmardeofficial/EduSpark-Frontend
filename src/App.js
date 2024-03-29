21ac9556ee0608369aec6f48dd5a61ee637fd1b2
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-03-29 11:04:28 +0530

    inputbackgroundcolorchanges

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes } from "react-router-dom";
-
 import AdminDashboard from "./components/admin/admindashboard/AdminDashboard";
 import TeacherDashboard from "./components/teacher/teacherdashboard/TeacherDashboard";
 
@@ -11,7 +10,7 @@ function App() {
       {role === "Admin" && <AdminDashboard />}
       {role === "Teacher" && <TeacherDashboard />}
 
-      {role == "" && <Routes></Routes>}
+      {role === "" && <Routes></Routes>}
     </Router>
   );
 }
